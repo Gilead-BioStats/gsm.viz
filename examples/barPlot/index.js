ba603ebe30bd6eb73a6a90fdd946cc993ad9faeb
Maya Gans <jaffe.maya@gmail.com>
2022-08-24 10:28:21 -0600

    adding more config

--- examples/barPlot/index.js
+++ examples/barPlot/index.js
@@ -6,8 +6,8 @@ fetch("../data/results_summary.csv")
       .filter((d) => d.workflowid === "kri0001")
       .map((d) => d.flag);
 
+    // this should be a function in a utils folder prob?
     var colors = [];
-
     let yellow = "#FADB14";
     let green = "#52C41A";
     let red = "#FF4D4F";
@@ -39,13 +39,76 @@ fetch("../data/results_summary.csv")
               y: parseInt(d.score),
             })),
           backgroundColor: colors,
-          responsive: true,
-          maintainAspectRatio: false,
         },
       ],
     };
 
-    console.log(chartData);
+    let options = {
+      plugins: {
+        datalabels: {
+          anchor: "end",
+          align: "top",
+          rotation: 90,
+          font: {
+            weight: "bold",
+          },
+          formatter: function (value, ctx) {
+            var index = ctx.dataIndex;
+            var label = ctx.chart.data.labels[index];
+            return label;
+          },
+        },
+        annotation: {
+          annotations: {
+            line1: {
+              type: "line",
+              yMin: 10,
+              yMax: 10,
+              borderColor: "red",
+              borderWidth: 2,
+              borderDash: [5],
+            },
+          },
+        },
+        legend: {
+          display: true,
+          position: "top",
+        },
+      },
+      scales: {
+        x: {
+          ticks: {
+            display: false,
+          },
+          grid: {
+            display: false,
+          },
+        },
+        y: {
+          title: {
+            display: true,
+            text: "AE Reporting Residual Score",
+            /*
+              color: "#911",
+              font: {
+                family: "Comic Sans MS",
+                size: 20,
+                weight: "bold",
+                lineHeight: 1.2,
+              },
+              */
+            padding: { top: 20, left: 0, right: 0, bottom: 0 },
+          },
+          grid: {
+            borderDash: [5],
+          },
+        },
+      },
+    };
 
-    const output = rbmViz.barPlot(document.getElementById("canvas"), chartData);
+    const output = rbmViz.barPlot(
+      document.getElementById("canvas"),
+      chartData,
+      options
+    );
   });
--- src/barPlot.js
+++ src/barPlot.js
@@ -1,64 +1,19 @@
 import Chart from "chart.js/auto";
 import ChartDataLabels from "chartjs-plugin-datalabels";
-// import { getRelativePosition } from "chart.js/helpers";
 
-export default function barPlot(ctx, data) {
+/**
+ * chart.js barchart wrapper function
+ * @param  {Node} arg1 HTML element to place plot within
+ * @param  {Object} arg2 data to be used in bar chart, categorical x-axis, numeric y-axis
+ * @param  {Object} arg3 the configuration for the plot including labels, annotations, colors
+ * @returns {Node} responsive barchart inside the delieniated container
+ */
+export default function barPlot(ctx, data, config) {
   const chart = new Chart(ctx, {
     type: "bar",
     data: data,
     plugins: [ChartDataLabels],
-    options: {
-      plugins: {
-        datalabels: {
-          anchor: "end",
-          align: "top",
-          rotation: 90,
-          font: {
-            weight: "bold",
-          },
-          formatter: function (value, ctx) {
-            var index = ctx.dataIndex;
-            var label = ctx.chart.data.labels[index];
-            return label;
-          },
-        },
-        annotation: {
-          annotations: {
-            line1: {
-              type: "line",
-              yMin: 10,
-              yMax: 10,
-              borderColor: "red",
-              borderWidth: 2,
-              borderDash: [5],
-            },
-          },
-        },
-      },
-      scales: {
-        x: {
-          ticks: {
-            display: false,
-          },
-        },
-        y: {
-          title: {
-            display: true,
-            text: "AE Reporting Residual Score",
-            /*
-            color: "#911",
-            font: {
-              family: "Comic Sans MS",
-              size: 20,
-              weight: "bold",
-              lineHeight: 1.2,
-            },
-            */
-            padding: { top: 20, left: 0, right: 0, bottom: 0 },
-          },
-        },
-      },
-    },
+    options: config,
   });
 
   return chart;
