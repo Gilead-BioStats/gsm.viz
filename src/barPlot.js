a58685c4edf749e13dec734e229c8cf7848aea4e
Maya Gans <jaffe.maya@gmail.com>
2022-09-06 13:57:47 -0700

    update plot

--- src/barPlot.js
+++ src/barPlot.js
@@ -1,8 +1,8 @@
-import Chart from "chart.js/auto";
-import addCanvas from "./util/addCanvas";
-import configure from "./barPlot/configure";
-import { rollups } from "d3";
-import customLegend from "./util/generateLegend";
+import Chart from 'chart.js/auto';
+import addCanvas from './util/addCanvas';
+import configure from './barPlot/configure';
+import { rollups } from 'd3';
+import generateLegend from './util/generateLegend';
 
 /**
  * chart.js barchart wrapper function
@@ -12,120 +12,123 @@ import customLegend from "./util/generateLegend";
  * @returns {Node} responsive barchart inside the delieniated container
  */
 export default function barPlot(_element_, _data_, _config_ = {}) {
-  const canvas = addCanvas(_element_);
+    const canvas = addCanvas(_element_);
 
-  // Update config.
-  const config = configure(_config_);
+    // Update config.
+    const config = configure(_config_);
 
-  const colors = {
-    green: "#52C41A",
-    yellow: "#FADB14",
-    red: "#FF4D4F",
-  };
+    const colors = {
+        green: '#52C41A',
+        yellow: '#FADB14',
+        red: '#FF4D4F',
+    };
 
-  // Update data.
-  const data = _data_
-    .map((d) => {
-      const datum = {
-        x: d[config.x],
-        y: +d[config.y],
-        stratum: Math.abs(+d[config.color]),
-      };
+    // Update data.
+    const data = _data_
+        .map((d) => {
+            const datum = {
+                x: d[config.x],
+                y: +d[config.y],
+                stratum: Math.abs(+d[config.color]),
+            };
 
-      return datum;
-    })
-    .sort((a, b) => a.stratum - b.stratum);
+            return datum;
+        })
+        .sort((a, b) => a.stratum - b.stratum);
 
-  // dummy dataset for legend
-  const lineLegend = [
-    {
-      type: "line",
-      label: "Flagged Threshold",
-      data: [],
-      borderColor: colors.red,
-    },
-    {
-      type: "line",
-      label: "At Risk Threshold",
-      data: [],
-      borderColor: colors.yellow,
-    },
-  ];
-
-  const datasets = rollups(
-    data,
-    (group) => {
-      return {
-        type: "bar",
-        label: `Flag=${group[0].stratum}`,
-        data: group,
-      };
-    },
-    (d) => d.stratum
-  ).map((group, i) => {
-    const dataset = group[1];
-    dataset.backgroundColor =
-      group[1].label === "Flag=0" ? colors.green : colors.red;
-    return dataset;
-  });
-
-  let plugins = {
-    datalabels: {
-      //anchor: "end",
-      //align: "top",
-
-      // anchor: "start",
-      // align: "bottom",
-      anchor: function (context) {
-        let y = context.dataset.data[context.dataIndex].y;
-        return y < 0 ? "end" : "start";
-      },
-      align: function (context) {
-        let y = context.dataset.data[context.dataIndex].y;
-        return y < 0 ? "top" : "bottom";
-      },
-      rotation: 90,
-      font: {
-        weight: "bold",
-      },
-      formatter: function (value, ctx) {
-        var index = ctx.dataIndex;
-        var label = ctx.chart.data.labels[index];
-        return label;
-      },
-    },
-    annotation: {
-      annotations: {
-        line1: {
-          drawTime: "beforeDatasetsDraw",
-          type: "line",
-          yMin: 10,
-          yMax: 10,
-          borderColor: "red",
-          borderWidth: 2,
-          borderDash: [5],
+    // dummy dataset for legend
+    const lineLegend = [
+        {
+            type: 'line',
+            label: 'Flagged Threshold',
+            data: [],
+            borderColor: colors.red,
         },
-      },
-    },
-    legend: {
-      display: false,
-    },
-  };
+        {
+            type: 'line',
+            label: 'At Risk Threshold',
+            data: [],
+            borderColor: colors.yellow,
+        },
+    ];
 
-  let scales = {
-    x: {
-      ticks: {
-        display: false,
-      },
-      grid: {
-        display: false,
-      },
-    },
-    y: {
-      title: {
-        display: true,
-        text: "AE Reporting Residual Score",
-        /*
+    const datasets = rollups(
+        data,
+        (group) => {
+            return {
+                type: 'bar',
+                label: `Flag=${group[0].stratum}`,
+                data: group,
+            };
+        },
+        (d) => d.stratum
+    ).map((group, i) => {
+        const dataset = group[1];
+        dataset.backgroundColor =
+            group[1].label === 'Flag=0' ? colors.green : colors.red;
+        return dataset;
+    });
+
+    console.log(datasets);
+
+    let plugins = {
+        datalabels: {
+            //anchor: "end",
+            //align: "top",
+
+            // anchor: "start",
+            // align: "bottom",
+            anchor: function (context) {
+                let y = context.dataset.data[context.dataIndex].y;
+                return y < 0 ? 'end' : 'start';
+            },
+            align: function (context) {
+                let y = context.dataset.data[context.dataIndex].y;
+                return y < 0 ? 'top' : 'bottom';
+            },
+            rotation: 90,
+            font: {
+                weight: 'bold',
+            },
+            formatter: function (value, ctx) {
+                var index = ctx.dataIndex;
+                var label = ctx.chart.data.labels[index];
+                return label;
+            },
+        },
+        annotation: {
+            annotations: {
+                line1: {
+                    drawTime: 'beforeDatasetsDraw',
+                    type: 'line',
+                    yMin: 10,
+                    yMax: 10,
+                    borderColor: 'red',
+                    borderWidth: 2,
+                    borderDash: [5],
+                },
+            },
+        },
+        legend: {
+            display: false,
+        },
+    };
+
+    let scales = {
+        x: {
+            type: 'category',
+            ticks: {
+                display: false,
+            },
+            grid: {
+                display: false,
+            },
+        },
+        y: {
+            title: {
+                display: true,
+                text: 'AE Reporting Residual Score',
+                /*
             color: "#911",
             font: {
               family: "Comic Sans MS",
@@ -134,24 +137,31 @@ export default function barPlot(_element_, _data_, _config_ = {}) {
               lineHeight: 1.2,
             },
         */
-        padding: { top: 20, left: 0, right: 0, bottom: 0 },
-      },
-      grid: {
-        borderDash: [5],
-      },
-    },
-  };
+                padding: { top: 20, left: 0, right: 0, bottom: 0 },
+            },
+            grid: {
+                borderDash: [5],
+            },
+        },
+    };
 
-  const options = {
-    plugins,
-    scales,
-  };
+    const options = {
+        plugins,
+        scales,
+    };
 
-  const chart = new Chart(canvas, {
-    data: { datasets: [...lineLegend, ...datasets] },
-    options,
-    plugins: [customLegend],
-  });
+    const customLegend = {
+        id: 'customLegend',
+        afterDraw(chart, args, options) {
+            generateLegend(chart, '.chartBox');
+        },
+    };
 
-  return chart;
+    const chart = new Chart(canvas, {
+        data: { datasets: [...lineLegend, ...datasets] },
+        options,
+        plugins: [customLegend],
+    });
+
+    return chart;
 }
--- src/util/generateLegend.js
+++ src/util/generateLegend.js
@@ -1,56 +1,52 @@
 function updateLegend(click, output) {
-  const element = click.target.parentNode;
-  element.classList.toggle("fade");
-  output.update();
+    const element = click.target.parentNode;
+    element.classList.toggle('fade');
+    console.log(output);
+    output.update();
 }
 
-function generateLegend(output, container) {
-  const chartBox = document.querySelector(container);
-  const div = document.createElement("DIV");
-  div.setAttribute("class", "customLegend");
+export default function generateLegend(output, container) {
+    if (document.querySelectorAll('.customLegend').length === 0) {
+        const chartBox = document.querySelector(container);
+        const div = document.createElement('DIV');
+        div.setAttribute('class', 'customLegend');
 
-  const ul = document.createElement("UL");
+        const ul = document.createElement('UL');
 
-  output.legend.legendItems.forEach((dataset, index) => {
-    const text = dataset.text;
-    const stroke = dataset.strokeStyle;
-    const fill = dataset.fillStyle;
-    const fontColor = "#666";
-    const dat = dataset.data;
+        output.legend.legendItems.forEach((dataset, index) => {
+            const text = dataset.text;
+            const stroke = dataset.strokeStyle;
+            const fill = dataset.fillStyle;
+            const fontColor = '#666';
+            const dat = dataset.data;
 
-    const li = document.createElement("LI");
-    const spanBox = document.createElement("SPAN");
-    spanBox.style.borderColor = stroke;
+            const li = document.createElement('LI');
+            const spanBox = document.createElement('SPAN');
+            spanBox.style.borderColor = stroke;
 
-    if (fill == "rgba(0,0,0,0.1)") {
-      spanBox.setAttribute("class", "legend-annotation");
-    } else {
-      spanBox.setAttribute("class", "legend-content");
-      spanBox.style.backgroundColor = fill;
+            if (fill == 'rgba(0,0,0,0.1)') {
+                spanBox.setAttribute('class', 'legend-annotation');
+            } else {
+                spanBox.setAttribute('class', 'legend-content');
+                spanBox.style.backgroundColor = fill;
+            }
+
+            const p = document.createElement('P');
+            const textNode = document.createTextNode(text);
+
+            li.onclick = (click) => {
+                const isHidden = !output.isDatasetVisible(index);
+                output.setDatasetVisibility(index, isHidden);
+                updateLegend(click, output);
+            };
+
+            ul.appendChild(li);
+            li.appendChild(spanBox);
+            li.appendChild(p);
+            p.appendChild(textNode);
+        });
+
+        chartBox.prepend(div);
+        div.appendChild(ul);
     }
-
-    const p = document.createElement("P");
-    const textNode = document.createTextNode(text);
-
-    li.onclick = (click) => {
-      const isHidden = !output.isDatasetVisible(index);
-      output.setDatasetVisibility(index, isHidden);
-      updateLegend(click);
-    };
-
-    ul.appendChild(li);
-    li.appendChild(spanBox);
-    li.appendChild(p);
-    p.appendChild(textNode);
-  });
-
-  chartBox.prepend(div);
-  div.appendChild(ul);
 }
-
-export const customLegend = {
-  id: "customLegend",
-  afterRender(chart, args, options) {
-    generateLegend(chart, ".chartBox");
-  },
-};
