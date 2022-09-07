7ca755b5a2a465a26c66d20c19e4b59f97fbf428
Maya Gans <jaffe.maya@gmail.com>
2022-09-07 09:29:34 -0700

    add configure code

--- src/barPlot/configure.js
+++ src/barPlot/configure.js
@@ -1,23 +1,23 @@
-import coalesce from "../util/coalesce";
+import coalesce from '../util/coalesce';
 
 export default function configure(_config_) {
-  const config = { ..._config_ };
-  console.log(config);
-  // x-axis
-  config.x = coalesce(config.x, "groupid");
-  config.xLabel = coalesce(config.xLabel, config[config.x]);
+    const config = { ..._config_ };
+    console.log(config);
+    // x-axis
+    config.x = coalesce(config.x, 'groupid');
+    config.xLabel = coalesce(config.xLabel, config[config.x]);
 
-  // y-axis
-  config.y = coalesce(config.y, "score");
-  config.yLabel = coalesce(config.yLabel, config[config.y]);
+    // y-axis
+    config.y = coalesce(config.y, 'score');
+    config.yLabel = coalesce(config.yLabel, config[config.y]);
 
-  // miscellaneous
-  config.color = coalesce(config.flag, "flag");
+    // miscellaneous
+    config.color = coalesce(config.flag, 'flag');
 
-  // n
-  config.n = coalesce(config.n, "n");
-  config.nLabel = coalesce(config.nLabel, config[config.n]);
+    // n
+    config.n = coalesce(config.n, 'n');
+    config.nLabel = coalesce(config.nLabel, config[config.n]);
 
-  console.log(config);
-  return config;
+    console.log(config);
+    return config;
 }
