f16b2d492309dcf86eacddf9e3d9ccfa3e4aa084
Spencer Childress <spencer.c.childress@gmail.com>
2022-09-13 10:55:35 -0400

    update sort of bars

--- src/barPlot/structureBarData.js
+++ src/barPlot/structureBarData.js
@@ -1,62 +1,63 @@
-import { rollups } from "d3";
+import { rollups } from 'd3';
 
 export default function structureBarData(_data_, config) {
-  const colors = {
-    green: "#52C41A",
-    yellow: "#FADB14",
-    red: "#FF4D4F",
-  };
+    const colors = {
+        green: '#52C41A',
+        yellow: '#FADB14',
+        red: '#FF4D4F',
+    };
 
-  // Update data.
-  const data = _data_
-    .map((d) => {
-      const datum = {
-        x: d[config.x],
-        y: +d[config.y],
-        stratum: Math.abs(+d[config.color]),
-        n: d[config.n],
-      };
+    // Update data.
+    const data = _data_
+        .map((d) => {
+            const datum = {
+                x: d[config.x],
+                y: +d[config.y],
+                stratum: +d[config.color],
+                n: d[config.n],
+            };
 
-      return datum;
-    })
-    .sort((a, b) => a.stratum - b.stratum);
+            return datum;
+        })
+        .sort((a, b) => b.y - a.y);
 
-  // dummy dataset for legend
-  const lineLegend = [
-    {
-      type: "line",
-      label: "Flagged Threshold",
-      data: [],
-      borderColor: colors.red,
-    },
-    {
-      type: "line",
-      label: "At Risk Threshold",
-      data: [],
-      borderColor: colors.yellow,
-    },
-  ];
+    // dummy dataset for legend
+    const lineLegend = [
+        {
+            type: 'line',
+            label: 'Flagged Threshold',
+            data: [],
+            borderColor: colors.red,
+        },
+        {
+            type: 'line',
+            label: 'At Risk Threshold',
+            data: [],
+            borderColor: colors.yellow,
+        },
+    ];
 
-  const datasets = rollups(
-    data,
-    (group) => {
-      return {
-        type: "bar",
-        label: `${
-          group[0].stratum === 0
-            ? "Sites Not Flagged or At Risk"
-            : "Flagged Site"
-        }`,
-        data: group,
-      };
-    },
-    (d) => d.stratum
-  ).map((group, i) => {
-    const dataset = group[1];
-    dataset.backgroundColor =
-      group[1].label === "Flagged Site" ? colors.red : colors.green;
-    return dataset;
-  });
+    const datasets = rollups(
+        data,
+        (group) => {
+            return {
+                type: 'bar',
+                label: `${
+                    group[0].stratum === 0
+                        ? 'Sites Not Flagged or At Risk'
+                        : 'Flagged Site'
+                }`,
+                data: group,
+                barThickness: 1.5,
+            };
+        },
+        (d) => d.stratum
+    ).map((group, i) => {
+        const dataset = group[1];
+        dataset.backgroundColor =
+            group[1].label === 'Flagged Site' ? colors.red : colors.green;
+        return dataset;
+    });
 
-  return [...lineLegend, ...datasets];
+    return [...lineLegend, ...datasets];
 }
