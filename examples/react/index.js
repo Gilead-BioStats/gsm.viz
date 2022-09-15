77fc743d1d4302b2b124f5e11b85c7631b66d7f4
Spencer Childress <spencer.c.childress@gmail.com>
2022-09-15 13:06:55 -0400

    fix #12

---
+++ examples/react/index.js
@@ -0,0 +1,95 @@
+const dataFiles = [
+    '../data/meta_workflow.csv',
+    '../data/results_summary.csv',
+    '../data/results_bounds.csv',
+];
+
+const dataPromises = dataFiles.map((dataFile) =>
+    fetch(dataFile).then((response) => response.text())
+);
+
+Promise.all(dataPromises)
+    .then((texts) => texts.map((text) => d3.csvParse(text)))
+    .then((datasets) => {
+        // data
+        const [workflow] = datasets[0] // destructured assignment
+            .sort((a, b) => d3.ascending(a.workflowid, b.workflowid));
+        const results = datasets[1].filter(
+            (d) => d.workflowid === workflow.workflowid
+        );
+        const bounds = datasets[2].filter(
+            (d) => d.workflowid === workflow.workflowid
+        );
+
+        // visualization
+        const instance = rbmViz.scatterPlot(
+            document.getElementById('container'),
+            results,
+            workflow,
+            bounds
+        );
+
+        // Handle data change event.
+        const kriDropdown = document.querySelector('#kri');
+        kriDropdown.addEventListener('change', (event) => {
+            const workflow = datasets[0].find(
+                (d) => d.workflowid === event.target.value
+            );
+            const results = datasets[1].filter(
+                (d) => d.workflowid === workflow.workflowid
+            );
+            const bounds = datasets[2].filter(
+                (d) => d.workflowid === workflow.workflowid
+            );
+            instance.helpers.updateData(instance, results, workflow, bounds);
+        });
+
+        // Handle config change event.
+        document
+            .querySelector('#x-axis-type')
+            .addEventListener('change', (event) => {
+                instance.helpers.updateOption(
+                    instance,
+                    'scales.x.type',
+                    event.target.value
+                );
+            });
+
+        const button = document.getElementById('destroy');
+        // Destroy chart:
+        // 1. calls chart.destroy
+        // 2. click event updates to create
+        // 3. button text changes to Create
+        const destroy = function () {
+            this.destroy();
+            button.innerHTML = '<em>Create</em>';
+            button.onclick = create;
+        };
+        button.onclick = destroy.bind(instance);
+
+        // Create chart:
+        // 1. calls rbmViz.scatterPlot
+        // 2. click event updates to destroy
+        // 3. button text changes to KILL
+        const create = () => {
+            const workflow = datasets[0].find(
+                (d) => d.workflowid === kriDropdown.value
+            );
+            const results = datasets[1].filter(
+                (d) => d.workflowid === workflow.workflowid
+            );
+            const bounds = datasets[2].filter(
+                (d) => d.workflowid === workflow.workflowid
+            );
+            const instance = rbmViz.scatterPlot(
+                document
+                    .getElementById('container')
+                    .getElementsByTagName('canvas')[0],
+                results,
+                workflow,
+                bounds
+            );
+            button.innerHTML = '<strong>KILL</strong>';
+            button.onclick = destroy.bind(instance);
+        };
+    });
