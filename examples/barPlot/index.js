51e65608b90f15e579f90f9ec6f9dc0175e3f352
Maya Gans <jaffe.maya@gmail.com>
2022-09-16 06:05:57 -0700

    scaffold click, doesnt work yet

--- examples/barPlot/index.js
+++ examples/barPlot/index.js
@@ -1,34 +1,34 @@
-const dataFiles = ['../data/meta_workflow.csv', '../data/results_summary.csv'];
+const dataFiles = ["../data/meta_workflow.csv", "../data/results_summary.csv"];
 
 const dataPromises = dataFiles.map((dataFile) =>
-    fetch(dataFile).then((response) => response.text())
+  fetch(dataFile).then((response) => response.text())
 );
 
 Promise.all(dataPromises)
-    .then((texts) => texts.map((text) => d3.csvParse(text)))
-    .then((datasets) => {
-        // data
-        const [workflow] = datasets[0] // destructured assignment
-            .sort((a, b) => d3.ascending(a.workflowid, b.workflowid));
-        const results = datasets[1].filter(
-            (d) => d.workflowid === workflow.workflowid
-        );
-        console.log(results);
-        // visualization
-        const instance = rbmViz.barPlot(
-            document.getElementById('container'),
-            results,
-            workflow
-        );
+  .then((texts) => texts.map((text) => d3.csvParse(text)))
+  .then((datasets) => {
+    // data
+    const [workflow] = datasets[0] // destructured assignment
+      .sort((a, b) => d3.ascending(a.workflowid, b.workflowid));
+    const results = datasets[1].filter(
+      (d) => d.workflowid === workflow.workflowid
+    );
 
-        // Handle data change event.
-        document.querySelector('#kri').addEventListener('change', (event) => {
-            const workflow = datasets[0].find(
-                (d) => d.workflowid === event.target.value
-            );
-            const results = datasets[1].filter(
-                (d) => d.workflowid === workflow.workflowid
-            );
-            instance.helpers.updateBarData(instance, results, workflow);
-        });
+    // visualization
+    const instance = rbmViz.barPlot(
+      document.getElementById("container"),
+      results,
+      workflow
+    );
+
+    // Handle data change event.
+    document.querySelector("#kri").addEventListener("change", (event) => {
+      const workflow = datasets[0].find(
+        (d) => d.workflowid === event.target.value
+      );
+      const results = datasets[1].filter(
+        (d) => d.workflowid === workflow.workflowid
+      );
+      instance.helpers.updateBarData(instance, results, workflow);
     });
+  });
