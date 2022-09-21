647f0a7ce9c30e7edfb9bae61a589ea2e930f41b
Spencer Childress <spencer.c.childress@gmail.com>
2022-09-20 21:45:17 -0400

    fix #67 - save CSV files as JSON

---
+++ examples/data/csv-to-json.js
@@ -0,0 +1,31 @@
+//jshint esversion:8
+//jshint node:true
+const fs = require( 'fs' );
+const path = require( 'path' );
+const csvToJson = require('convert-csv-to-json');
+
+// Make an async function that gets executed immediately
+(async ()=>{
+    // Our starting point
+    try {
+        // Get the files as an array
+        const files = await fs.promises.readdir( '.' );
+
+        // Loop them all with the new for...of
+        for (const file of files) {
+            if (/csv$/.test(file)) {
+                console.log(file);
+                csvToJson
+                    .fieldDelimiter(',')
+                    .generateJsonFileFromCsv(
+                        file,
+                        file.replace(/csv$/, 'json')
+                    );
+            }
+        } // End for...of
+    }
+    catch( e ) {
+        // Catch anything bad that happens
+        console.error( "We've thrown! Whoops!", e );
+    }
+})(); // Wrap in parenthesis and call now
