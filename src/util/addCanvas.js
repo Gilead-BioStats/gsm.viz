a77b9bece3310b701722f781ef3c1fa88ea89c95
Spencer Childress <spencer.c.childress@gmail.com>
2022-10-05 22:56:42 -0400

    fix #77

--- src/util/addCanvas.js
+++ src/util/addCanvas.js
@@ -2,24 +2,30 @@ import addCustomHoverEvent from './addCanvas/addCustomHoverEvent';
 import addCustomClickEvent from './addCanvas/addCustomClickEvent';
 
 export default function addCanvas(_element_, config) {
-    if (!document.body.contains(_element_)) {
+    let canvas;
+
+    if (!document.body.contains(_element_)) { // [ _element_ ] doesn't exist
         console.error('addCanvas: [ _element_ ] does not exist.');
         return;
-    } else if (_element_.nodeName && _element_.nodeName === 'CANVAS') {
-        return _element_;
-    }
+    } else if (_element_.nodeName && _element_.nodeName === 'CANVAS') { // [ _element_ ] is a canvas element
+        canvas = _element_;
+    } else { // create a canvas element
+        const newCanvas = document.createElement('canvas');
+        const oldCanvas = _element_.getElementsByTagName('canvas')[0];
 
-    const newCanvas = document.createElement('canvas');
-    const oldCanvas = _element_.getElementsByTagName('canvas')[0];
-    if (canvas !== undefined) {
-        oldCanvas.replaceWith(newCanvas);
-    } else {
-        _element_.appendChild(newCanvas);
+        if (oldCanvas !== undefined) {
+            oldCanvas.replaceWith(newCanvas);
+        } else {
+            _element_.appendChild(newCanvas);
+        }
+
+        canvas = newCanvas;
     }
 
     config.hoverEvent = addCustomHoverEvent(canvas, config.hoverCallback);
     config.clickEvent = addCustomClickEvent(canvas, config.clickCallback);
+
     console.log(canvas);
 
-    return newCanvas;
+    return canvas;
 }
