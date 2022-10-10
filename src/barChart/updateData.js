572f467ccf264b2fe843821a40d3ea956422e357
Spencer Childress <spencer.c.childress@gmail.com>
2022-10-09 21:04:37 -0400

    fix state issue

--- src/barChart/updateData.js
+++ src/barChart/updateData.js
@@ -1,8 +1,13 @@
 import updateConfig from './updateConfig';
+import addCustomHoverEvent from '../util/addCanvas/addCustomHoverEvent';
+import addCustomClickEvent from '../util/addCanvas/addCustomClickEvent';
 import structureData from './structureData';
 
 export default function updateData(chart, _data_, _config_) {
     chart.data.config = updateConfig(chart, _config_);
+    // TODO: figure out why these events have to be redefined on data change
+    chart.data.config.hoverEvent = addCustomHoverEvent(chart.canvas, chart.data.config.hoverCallback);
+    chart.data.config.clickEvent = addCustomClickEvent(chart.canvas, chart.data.config.clickCallback);
     chart.data.datasets = structureData(_data_, chart.data.config);
     chart.update();
 }
--- src/scatterPlot/updateData.js
+++ src/scatterPlot/updateData.js
@@ -1,4 +1,6 @@
 import updateConfig from './updateConfig';
+import addCustomHoverEvent from '../util/addCanvas/addCustomHoverEvent';
+import addCustomClickEvent from '../util/addCanvas/addCustomClickEvent';
 import structureData from './structureData';
 
 /**
@@ -7,11 +9,14 @@ import structureData from './structureData';
  * @param {Object} chart - Chart.js chart object
  * @param {Array} _data_ - input data where each array item is an object of key-value pairs
  * @param {Object} _config_ - chart configuration and metadata
- * @param {Array} bounds - optional auxiliary data plotted as a line representing bounds
+ * @param {Array} _bounds_ - optional auxiliary data plotted as a line representing bounds
  *
  */
-export default function updateData(chart, _data_, _config_, bounds) {
+export default function updateData(chart, _data_, _config_, _bounds_) {
     chart.data.config = updateConfig(chart, _config_);
-    chart.data.datasets = structureData(_data_, chart.data.config, bounds);
+    // TODO: figure out why these events have to be redefined on data change
+    chart.data.config.hoverEvent = addCustomHoverEvent(chart.canvas, chart.data.config.hoverCallback);
+    chart.data.config.clickEvent = addCustomClickEvent(chart.canvas, chart.data.config.clickCallback);
+    chart.data.datasets = structureData(_data_, chart.data.config, _bounds_);
     chart.update();
 }
