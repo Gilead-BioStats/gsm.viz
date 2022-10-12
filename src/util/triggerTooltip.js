fc543022c9cb3d6b2f46e19981bb4804943fa012
Spencer Childress <spencer.c.childress@gmail.com>
2022-10-12 16:18:34 -0400

    git add -A

---
+++ src/util/triggerTooltip.js
@@ -0,0 +1,23 @@
+export default function triggerTooltip(chart) {
+    const tooltip = chart.tooltip;
+
+    if (tooltip.getActiveElements().length > 0) {
+        tooltip.setActiveElements([], {x: 0, y: 0});
+    }
+    
+    if (chart.data.config.selectedGroupIDs.length > 0) {
+        const pointIndex = chart.data.datasets[0].data
+            .findIndex(d => chart.data.config.selectedGroupIDs.includes(d.groupid));
+
+        tooltip.setActiveElements(
+            [
+                {
+                    datasetIndex: 0,
+                    index: pointIndex,
+                },
+            ],
+        );
+    }
+
+    chart.update();
+}
