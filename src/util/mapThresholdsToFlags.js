3c19e5e51aca2bbb2ef638c107a728fc2600e087
Paritosh Mahale <paritosh.mahale@zs.com>
2022-12-06 15:28:27 +0530

    Fix issue

--- src/util/mapThresholdsToFlags.js
+++ src/util/mapThresholdsToFlags.js
@@ -1,29 +1,28 @@
+import { ascending } from "d3";
 export default function mapThresholdsToFlags(_thresholds_, complete = true) {
-    // Capture complete set of thresholds, even when only a partial set exists in the data.
-    let thresholds = _thresholds_
-        .map((threshold) => +threshold)
-        .sort(d3.ascending);
+  // Capture complete set of thresholds, even when only a partial set exists in the data.
+  let thresholds = _thresholds_.map((threshold) => +threshold).sort(ascending);
 
-    //if (complete) {
-    //    const nonNegativeThresholds = [
-    //        ...new Set(_thresholds_.map((threshold) => Math.abs(threshold))),
-    //    ];
-    //    const negativeThresholds = nonNegativeThresholds.map(
-    //        (threshold) => -1 * threshold
-    //    );
-    //    thresholds = [
-    //        ...new Set([...nonNegativeThresholds, ...negativeThresholds]),
-    //    ].sort((a, b) => a - b);
-    //}
+  //if (complete) {
+  //    const nonNegativeThresholds = [
+  //        ...new Set(_thresholds_.map((threshold) => Math.abs(threshold))),
+  //    ];
+  //    const negativeThresholds = nonNegativeThresholds.map(
+  //        (threshold) => -1 * threshold
+  //    );
+  //    thresholds = [
+  //        ...new Set([...nonNegativeThresholds, ...negativeThresholds]),
+  //    ].sort((a, b) => a - b);
+  //}
 
-    // Define sequence centered at 0.
-    const flags = thresholds.map((threshold, i) => {
-        const flag = i - Math.floor(thresholds.length / 2);
-        return {
-            threshold,
-            flag: flag + (thresholds.length % 2 === 0 && flag >= 0), // Hack that
-        };
-    });
+  // Define sequence centered at 0.
+  const flags = thresholds.map((threshold, i) => {
+    const flag = i - Math.floor(thresholds.length / 2);
+    return {
+      threshold,
+      flag: flag + (thresholds.length % 2 === 0 && flag >= 0), // Hack that
+    };
+  });
 
-    return flags;
+  return flags;
 }
