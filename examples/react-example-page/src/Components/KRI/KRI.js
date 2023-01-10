520d3c16ad16fef16a833372b7e97f0206f94d69
Maya Gans <maya.gans@atorusresearch.com>
2023-01-10 09:52:57 -0800

    adding country to KRI dropdown

--- examples/react-example-page/src/Components/KRI/KRI.js
+++ examples/react-example-page/src/Components/KRI/KRI.js
@@ -8,7 +8,9 @@ import workflows from '../../data/meta_workflow';
 // TODO: on KRI change current state of y-axis and threshold toggle are not effected
 const KRI = ({kri, setKri}) => {
 
-    let allKris = workflows.filter((d) => /^kri/.test(d.workflowid))
+    console.log(workflows)
+
+    let allKris = workflows.filter((d) => /^kri|^cou/.test(d.workflowid))
     // observe KRI dropdown
     const handleKriChange = (event) => {
         setKri(event.target.value);
@@ -17,7 +19,7 @@ const KRI = ({kri, setKri}) => {
     return (
             <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
                 <InputLabel id="demo-simple-select-standard-label">
-                    KRI
+                    KRI + Country
                 </InputLabel>
                 <Select
                     labelId="demo-simple-select-standard-label"
@@ -29,7 +31,7 @@ const KRI = ({kri, setKri}) => {
                     {allKris.map((d, index) => {
                         return (
                             <MenuItem key={index} value={d.workflowid}>
-                                {d.metric}
+                                {d.metric} ({d.workflowid})
                             </MenuItem>
                         );
                     })}
