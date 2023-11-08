110f9434b4a17d2a1ac0a4b113b762f66da198eb
Spencer Childress <spencer.c.childress@gmail.com>
2023-11-08 11:07:06 -0500

    spencer\'s tweaks

--- src/siteSummaryTable.js
+++ src/siteSummaryTable.js
@@ -1,22 +1,29 @@
-import data from './summary-data.json';
+import summaryData from './summary-data.json' assert { type: "json" };
 
-const makeSiteSummaryData = () => {
-    data.site_grid_details.map((item, i) => {
-        let obj = {
-            site_id: item.site_id,
-            enrolled_subjects: item.subject_enrolled,
-            red_kris: item.at_risk_kri,
-            amber_kris: item.flagged_kri,
-        };
+const makeSiteSummaryData = (data) => {
+    return(
+        data.site_grid_details.map((item, i) => {
+            let obj = {
+                site_id: item.site_id,
+                enrolled_subjects: item.subject_enrolled,
+                red_kris: item.at_risk_kri,
+                amber_kris: item.flagged_kri,
+            };
 
-        item.kri_column_details.map((kriItem, i) => {
-            return (kriObj = {
-                kriItem,
-            });
-        });
+            let kriObj = item.kri_column_details
+                // TODO: sort from kri0001 to kri0012
+                .sort((a,b) => (
+                    a[Object.keys(a)] < b[Object.keys(b)] ? -1 :
+                    b[Object.keys(b)] < a[Object.keys(a)] ?  1 : 0
+                ))
+                .reduce((kriItems, kriItem, i) => {
+                    kriItems[Object.keys(kriItem)] = kriItem[Object.keys(kriItem)].flag_value;
+                    return kriItems;
+                }, {});
 
-        return [{ ...obj, ...kriObj }];
-    });
+            return { ...obj, ...kriObj };
+        })
+    );
 };
 
-console.log('result: ', makeSiteSummaryData);
+console.log('result: ', makeSiteSummaryData(summaryData));
