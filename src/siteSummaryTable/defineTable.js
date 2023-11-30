a4bdbb31d0b725103107a807f36a29d1a67360b6
Mariana Zamarripa (Contractor) <Mariana.Zamarripa@gilead.com>
2023-11-30 13:14:41 -0800

    Migrate modularized react components into rbm-viz src to export them in gismo ui

---
+++ src/siteSummaryTable/defineTable.js
@@ -0,0 +1,20 @@
+import { useState, useMemo } from 'react';
+import { getCoreRowModel, useReactTable, getSortedRowModel } from '@tanstack/react-table';
+import makeSiteSummaryData from './components/summaryTable';
+
+export default function DefineTable(data, columns) {
+    const [sorting, setSorting] = useState([]);
+
+    const table = useReactTable({
+        data: useMemo(() => makeSiteSummaryData(data), []),
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        state: {
+            sorting: sorting,
+        },
+        onSortingChange: setSorting,
+    });
+
+    return(table);
+}
---
+++ src/summaryTableApp.js
@@ -0,0 +1,37 @@
+import SUMMARY_DATA from './summary-data.json';
+import './App.css';
+
+import defineKriObj from './app/defineKriObj.js';
+import defineColumns from './app/defineColumns.js';
+import DefineTable from './app/defineTable.js';
+import defineThead from './app/defineThead.js';
+import defineTbody from './app/defineTbody.js';
+
+function SummaryTableApp() {
+  //Assign KRI elements into their values
+  const kriObj = defineKriObj(SUMMARY_DATA);
+  
+  //Structure for our headers/colums keys
+  const columns = defineColumns(kriObj);
+
+  //Structure for our TanStack table
+  const table = DefineTable(SUMMARY_DATA, columns);
+
+  return (
+    <div className="App">
+      <h3>Site Summary Table</h3>
+      <div className='table-container'>
+        <table className='table'>
+          <thead>
+            { defineThead(table) }
+          </thead>
+          <tbody>
+            { defineTbody(table) }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default SummaryTableApp;
